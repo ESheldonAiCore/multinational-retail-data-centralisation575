aa8b558625dcef209f1d1e5765dfe036f47cea97
Shelduck <157506488+ESheldonAiCore@users.noreply.github.com>
2024-02-12 15:57:40 +0100

    Creates methods in Database Connector class

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,10 @@
+import yaml
+
 class DatabaseConnector:
     def __init__(self):
-        pass
+        return self
+
+    def read_db_creds(self):
+        with open('db_creds.yaml', 'r') as file:
+            database_creds = yaml.safe_load(file)
+            print(database_creds)
