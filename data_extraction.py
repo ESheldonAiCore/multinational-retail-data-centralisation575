43115dd419f2cd7779006204255828583638ea0f
Shelduck <157506488+ESheldonAiCore@users.noreply.github.com>
2024-02-13 17:57:46 +0100

    adding methods to create db engine and list tables

--- data_extraction.py
+++ data_extraction.py
@@ -1,3 +1,7 @@
 class DataExtractor:
     def __init__(self):
         pass
+
+    def list_db_tables(self,engine):
+        list_tables = metadata.reflect(engine)
+        return list_tables
--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,6 @@
 import yaml
+from sqlalchemy import create_engine as eng
+from sqlalchemy import URL
 
 class DatabaseConnector:
     def __init__(self):
@@ -8,3 +10,15 @@ class DatabaseConnector:
         with open('db_creds.yaml', 'r') as file:
             database_creds = yaml.safe_load(file)
             return database_creds
+
+    def init_db_engine(self):
+        credentials = self.read_db_creds()
+        url_object = URL.create(
+            "postgresql",
+            username=credentials['RDS_USER'],
+            password=credentials['RDS_PASSWORD'], 
+            host=credentials['RDS_HOST']
+            database=credentials['RDS_DATABASE'],
+            )
+        engine = eng.create_engine(url_object)
+        return engine
